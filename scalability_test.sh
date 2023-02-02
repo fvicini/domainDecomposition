aa3efeb22a090e3b2ebeedd350820367c45caea2
fabio.vicini <fabio.vicini@polito.it>
2023-02-02 14:03:52 +0100

    add: mesh size

--- scalability_test.sh
+++ scalability_test.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
-mpirun --oversubscribe -np 1 DOMAIN_DECOMPOSITION ExportFolder:string="./Run_N1" MeshParameter:double=0.0625 SchurSolverMaxIterations:uint=100 SchurSolverTolerance:double=1e-16 SchurSolverType:bool=1
-mpirun --oversubscribe -np 4 DOMAIN_DECOMPOSITION ExportFolder:string="./Run_N4" MeshParameter:double=0.0625 SchurSolverMaxIterations:uint=100 SchurSolverTolerance:double=1e-16 SchurSolverType:bool=1
-mpirun --oversubscribe -np 16 DOMAIN_DECOMPOSITION ExportFolder:string="./Run_N16" MeshParameter:double=0.0625 SchurSolverMaxIterations:uint=100 SchurSolverTolerance:double=1e-16 SchurSolverType:bool=1
+meshSize=$1
+
+mpirun --oversubscribe -np 1 DOMAIN_DECOMPOSITION ExportFolder:string="./Run_N1" MeshParameter:double=$meshSize SchurSolverMaxIterations:uint=100 SchurSolverTolerance:double=1e-16 SchurSolverType:bool=1
+mpirun --oversubscribe -np 4 DOMAIN_DECOMPOSITION ExportFolder:string="./Run_N4" MeshParameter:double=$meshSize SchurSolverMaxIterations:uint=100 SchurSolverTolerance:double=1e-16 SchurSolverType:bool=1
+mpirun --oversubscribe -np 16 DOMAIN_DECOMPOSITION ExportFolder:string="./Run_N16" MeshParameter:double=$meshSize SchurSolverMaxIterations:uint=100 SchurSolverTolerance:double=1e-16 SchurSolverType:bool=1
